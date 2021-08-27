3c187df422f321ab8e2009026fd2c962eef9493c
developer-ji <72510258+developer-ji@users.noreply.github.com>
2021-08-27 17:03:14 +0530

    Add files via upload

---
+++ square_and_qube.py
@@ -0,0 +1,13 @@
+square = []
+qube = []
+for i in range(1,11):
+    square.append(i**2)
+    if i in range(1,11):
+        qube.append(i**3)
+
+    
+print("square = ",square)
+print("qube = ",qube)
+
+
+
