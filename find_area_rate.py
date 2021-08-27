74a906ee9c5313703b13852a8ea3b29048342be8
developer-ji <72510258+developer-ji@users.noreply.github.com>
2021-08-27 16:59:28 +0530

    Add files via upload

---
+++ find_area_rate.py
@@ -0,0 +1,15 @@
+x = float(input("Enter of area length : "))
+y = float(input("Enter of area width : "))
+s = int(input("Enter of rate : "))
+z =(x*y)
+print(f"area of = {z},and price of property= {z*s}")
+
+
+
+
+# area calculate 
+x,y,z = (30,40,450) # this is my property rewa 30by40 price 450
+r = float(x*y)
+print(f"area of = {r},  and  price of property = {r*z}")
+
+#f formarting me python3.6 version ka use hoga 
